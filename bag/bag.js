886156159a277c7403e7fd08d99efa61434d4794
RIYAS <riyass973@gmail.com>
2022-08-24 23:29:02 +0530

    day-2 bag

---
+++ bag/bag.js
@@ -0,0 +1,3 @@
+import { navabar } from "./components/navbar.js";
+let x = document.querySelector('.navbar');
+x.innerHTML = navabar();
---
+++ bag/components/navbar.js
@@ -0,0 +1,29 @@
+function navabar(){
+    return `
+    <div class="mylogo">
+        <img src="https://seeklogo.com/images/M/myntra-logo-B3C45EAD5C-seeklogo.com.png" alt="mythra-logo" />
+    </div>
+    <div class="qute">
+        <ol>
+            BAG
+        </ol>
+        <ol>
+            ----------
+        </ol>
+        <ol>
+            ADDRESS
+        </ol>
+        <ol>
+            ----------
+        </ol>
+        <ol>
+            PAYMENT
+        </ol>
+    </div>
+    <div class="sec">
+        <img src="https://constant.myntassets.com/checkout/assets/img/sprite-secure.png" />
+        <p>100% SECURE</p>
+    </div>
+`
+}
+export {navabar}
